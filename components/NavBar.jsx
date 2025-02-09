ce68d8d4c1ae8610c436cc17f80ba39f7e928586
Jimmy Johnson <Daniel@Berylbrothers.com>
2025-02-09 12:54:36 -0800

    Nav Bar Changes

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -5,19 +5,21 @@ import Link from 'next/link'
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav className="w-full fixed px-5 py-4 flex items-center justify-between">
     <Image src={"/logo-transparent.png"}
     height={100}
     width={100}
     alt=""
     />
-    <ul className='flex flex-row justify-center items-center '>
+    <ul className='flex justify-between space-x-6'>
         <li>Home</li>
         <li>About</li>
         <li>Services</li>
         <li>Work</li>
         <li>Contact</li>
     </ul>
+
+    <div>Contact</div>
     </nav>
   )
 }
